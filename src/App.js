86f1c0280a0bf2ca99dad9309dba295a207a7ad5
john Mohebbi <mohebbijohn@gmail.com>
2022-01-27 13:12:11 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default App;
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  separator: ':',
+  important: true,
+  
+ 
+  theme: {
+    screens:{
+      'xs':"450px",
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily:{
+        "bYakan":'bYakan',
+        "vazir":'vazir10',
+      },
+     
+    },
+  },
+  plugins: [
+  ],
+};
